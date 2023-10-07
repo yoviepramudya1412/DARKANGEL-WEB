49ddfd8e0a141ee4d570c18ffd164fac9f7df84f
YoviePramudya <yoviepramudyasabri@gmail.com>
2023-10-07 16:52:04 +0700

    ubah token

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/',views.test,name='test'),
+    path('',views.test,name='test'),
+    path('api/dosen/', views.DosenListCreateView.as_view(), name='dosen-list-create'),
+    path('api/pengolahan/', views.PengolahanListCreateView.as_view(), name='pengolahan-list-create'),
+    path('api/absensi/',views.AbsensiListCreateView.as_view(), name='absensi-list-create'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Dosen, Pengolahan, Absensi
+from .serializers import DosenSerializer, PengolahanSerializer, AbsensiSerializer
+
+class DosenListCreateView(generics.ListCreateAPIView):
+    queryset = Dosen.objects.all()
+    serializer_class = DosenSerializer
+
+class PengolahanListCreateView(generics.ListCreateAPIView):
+    queryset = Pengolahan.objects.all()
+    serializer_class = PengolahanSerializer
+
+class AbsensiListCreateView(generics.ListCreateAPIView):
+    queryset = Absensi.objects.all()
+    serializer_class = AbsensiSerializer
+
 
 # Create your views here.
 def test(request):
