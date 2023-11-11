a3a50331dbe76a3f1ed6ec3918664d6f9703d26c
YoviePramudya <yoviepramudyasabri@gmail.com>
2023-11-11 18:21:51 +0700

    Major perubahan dari yovie, API login dan Model

--- core/models.py
+++ core/models.py
@@ -1,52 +1,115 @@
 import uuid
+from django.contrib.auth.hashers import make_password
+from django.utils import timezone
+
+
+
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
-# acckey serve --yoviepramudya
+class CustomUserManager(BaseUserManager):
+    def create_user(self, nip, nama, umur, golongan, password=None):
+        if not nip:
+            raise ValueError('NIP field must be set')
+        user = self.model(
+            nip=nip,
+            nama=nama,
+            umur=umur,
+            golongan=golongan,
+        )
+        user.set_password(password)  # hash password
+        user.save(using=self._db)
+        return user
 
+    def create_superuser(self, nip, nama, umur, golongan, password=None):
+        user = self.create_user(
+            nip=nip,
+            nama=nama,
+            umur=umur,
+            golongan=golongan,
+            password=password,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
-class Dosen(models.Model):
-    id = models.AutoField(primary_key=True)
-    nip = models.CharField(max_length=14, unique=True)
-    nama_dosen = models.CharField(max_length=255)
-    umur = models.PositiveIntegerField()
-    golongan = models.CharField(max_length=10, blank=True, null=True)
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    
+    nip = models.CharField(max_length=20, unique=True)
+    nama = models.CharField(max_length=255)
+    umur = models.IntegerField()
+    golongan = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+
+    # Use NIP as the username field
+    USERNAME_FIELD = 'nip'
+    REQUIRED_FIELDS = ['nama', 'umur', 'golongan']
 
     def __str__(self):
-        return self.nama_dosen
+        return self.nama
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+
+
+
+
+
+
 
 def generate_filename(instance, filename):
     ext = filename.split('.')[-1]
     filename = f"{uuid.uuid4()}.{ext}"
     return f'sampel/{filename}'
+def generate_filename2(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return f'training/{filename}'
 
 class Pengolahan(models.Model):
     id = models.AutoField(primary_key=True)
-    dosen = models.OneToOneField(Dosen, on_delete=models.CASCADE)
-    sampel_1 = models.ImageField(upload_to=generate_filename)
-    sampel_2 = models.ImageField(upload_to=generate_filename)
-    sampel_3 = models.ImageField(upload_to=generate_filename)
-    sampel_4 = models.ImageField(upload_to=generate_filename)
-    sampel_5 = models.ImageField(upload_to=generate_filename)
-    sampel_6 = models.ImageField(upload_to=generate_filename)
-    sampel_7 = models.ImageField(upload_to=generate_filename)
-    sampel_8 = models.ImageField(upload_to=generate_filename)
-    sampel_9 = models.ImageField(upload_to=generate_filename)
-    sampel_10 = models.ImageField(upload_to=generate_filename)
-    sampel_11 = models.ImageField(upload_to=generate_filename)
-    profil = models.ImageField(upload_to='profil/')
+    staff = models.name = models.ForeignKey(CustomUser, related_name='pengolahan', on_delete=models.CASCADE)
+    sampel_1 = models.ImageField(upload_to=generate_filename, null=True, blank=True)
+    sampel_2 = models.ImageField(upload_to=generate_filename2, null=True, blank=True)
+    profil = models.ImageField(upload_to='profil/', null=True, blank=True)
 
     def __str__(self):
-        return self.dosen.nama_dosen
+        return self.staff.nama 
     
+    
+    
+
 class Absensi(models.Model):
+    ABSENCE_STATUS_CHOICES = [
+        ('belum absen', 'Belum Absen'),
+        ('sudah absen', 'Sudah Absen'),
+    ]
+
     id = models.AutoField(primary_key=True)
-    dosen = models.ForeignKey(Dosen, on_delete=models.CASCADE)
+    staff = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='absensi')
     pengolahan = models.ForeignKey(Pengolahan, on_delete=models.CASCADE)
-    tanggal_absensi = models.DateTimeField()
-    status_absensi = models.CharField(max_length=255)
-    sisa_absensi = models.IntegerField()
-    berapa_kali_absensi = models.IntegerField()
+    tanggal_absensi = models.DateTimeField(default=timezone.now)
+    status_absensi = models.CharField(max_length=255, choices=ABSENCE_STATUS_CHOICES, default='sudah absen')
+    sisa_absensi = models.IntegerField(default=0)
+    berapa_kali_absensi = models.IntegerField(default=2)
+
+    def save(self, *args, **kwargs):
+        # Menghitung sisa_absensi berdasarkan status_absensi dan berapa_kali_absensi
+        if self.status_absensi == 'sudah absen':
+            self.sisa_absensi = self.berapa_kali_absensi - 1
+        else:
+            self.sisa_absensi = self.berapa_kali_absensi
+        
+        super(Absensi, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.dosen.nama_dosen} - {self.tanggal_absensi}"
+        return f"{self.staff.nama} - {self.tanggal_absensi}"
--- core/views.py
+++ core/views.py
@@ -1,19 +1,185 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Dosen, Pengolahan, Absensi
-from .serializers import DosenSerializer, PengolahanSerializer, AbsensiSerializer
 
-class DosenListCreateView(generics.ListCreateAPIView):
-    queryset = Dosen.objects.all()
-    serializer_class = DosenSerializer
 
-class PengolahanListCreateView(generics.ListCreateAPIView):
-    queryset = Pengolahan.objects.all()
-    serializer_class = PengolahanSerializer
 
-class AbsensiListCreateView(generics.ListCreateAPIView):
-    queryset = Absensi.objects.all()
-    serializer_class = AbsensiSerializer
+
+from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .models import CustomUser,Pengolahan
+from .serializers import CustomUserSerializer,PengolahanSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from deepface import DeepFace
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.core.files.base import ContentFile
+
+import numpy as np
+import cv2
+import io
+
+
+
+from PIL import Image
+from io import BytesIO
+from django.http import JsonResponse
+import base64
+
+from django.db.models import Count, Avg
+
+import os
+from pathlib import Path
+
+
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def face_recognition_api(request):
+    print(request.data)
+
+    # Ensure the request has an image file
+    if 'sampel_1' not in request.FILES:
+        return Response({'error': 'Tidak ada file gambar yang diberikan'}, status=400)
+
+    # Get all instances of 'Pengolahan' for the authenticated user
+    pengolahan_instances = get_list_or_404(Pengolahan, staff=request.user)
+
+    # Get the uploaded image
+    uploaded_image = request.FILES['sampel_1']
+
+    # Create a temporary directory if it does not exist
+    temporary_directory = 'temp'
+    os.makedirs(temporary_directory, exist_ok=True)
+
+    # Save the temporary file to the file system
+    temporary_image_path = Path(temporary_directory) / uploaded_image.name
+    with open(temporary_image_path, 'wb+') as destination:
+        for chunk in uploaded_image.chunks():
+            destination.write(chunk)
+
+    # Perform face detection and save the result
+    
+
+
+
+    # List to store verification results for each Pengolahan instance
+    verified_list = []
+
+    for pengolahan_instance in pengolahan_instances:
+        image_path = Path(pengolahan_instance.sampel_2.path)
+
+        print(f"image_path: {image_path}")
+        print(f"uploaded_image_name: {uploaded_image.name}")
+
+        try:
+            # Perform face verification using deepface with the saved detection result
+            result = DeepFace.verify(
+                img1_path=str(image_path),
+                img2_path=str(temporary_image_path),
+                model_name="VGG-Face",
+                distance_metric='cosine',
+                enforce_detection=False,
+                detector_backend='opencv',
+                align=True,
+                normalization='base',
+            )
+
+            # Extract relevant information from the result
+            verified = result['verified']
+            verified_list.append(verified)
+
+        except Exception as e:
+            print(f"Error in deepface.verify: {str(e)}")
+            return Response({'error': str(e)}, status=500)
+
+    # Remove the temporary file after use
+    temporary_image_path.unlink()
+
+    # Calculate the average of the verification results
+    average_verified = sum(verified_list) / len(verified_list) if verified_list else 0.0
+
+    # Determine the overall verification status
+    overall_verified = average_verified >= 0.71  # Adjust the threshold as needed
+
+    # Return the response
+    response_data = {
+        'verified': overall_verified,
+        'average_distance': average_verified,
+    }
+
+    return Response(response_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
+def api_login(request):
+    if request.method == 'POST':
+        nip = request.data.get('nip')
+        password = request.data.get('password')
+
+        try:
+            user = CustomUser.objects.get(nip=nip)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'Invalid NIP or password'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+            serializer = CustomUserSerializer(user)  # Serialisasi data pengguna
+            return Response({
+                'access_token': access_token,
+                'refresh_token': refresh_token,
+                'user': serializer.data  # Menambahkan data pengguna ke respons
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid NIP or password'}, status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response({'error': 'Invalid request method'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@authentication_classes(['rest_framework_simplejwt.authentication.JWTAuthentication'])
+@permission_classes(['rest_framework.permissions.IsAuthenticated'])
+def staff_api(request):
+    CustomUser = CustomUser.objects.all()
+    serializer = CustomUserSerializer(CustomUser, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class StaffDetail(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
 
 
 # Create your views here.
